d7558d2d5f217c9822abc505c6e1f41d98922b3f
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-02-27 00:07:30 +0530

    add index.js file, searchBar.jsx file in components folder and add utils folder

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 
+import { Navbar, Feed, ChannelDetail, SearchFeed, VideoDetail } from './components';
+
 const App = () => (
     <BrowserRouter>
         <Box sx={{ backgroundColor: '#000'}}>
-            Navbar
+            <Navbar />
             <Routes>
                 <Route path='/' exact element={<Feed />} />
-                <Route path="/video/:id" element={<videoDetail />} />
-                <Route path="/channel/:id" element={<channelDetail />} />
-                <Route path="/search/:searchTerm" element={<searchFeed />} />
+                <Route path="/video/:id" element={<VideoDetail />} />
+                <Route path="/channel/:id" element={<ChannelDetail />} />
+                <Route path="/search/:searchTerm" element={<SearchFeed />} />
             </Routes> 
         </Box>
     </BrowserRouter>
